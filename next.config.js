3c0f561c0a3107e189c86054c16a0522f9a4ba70
Rashmi Suralkar <prashmi.89@gmail.com>
2025-08-10 13:31:40 +0100

    fixed docs folder

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  distDir: "docs",
   eslint: {
     ignoreDuringBuilds: true,
   },
+
+  assetPrefix: "",
   images: { unoptimized: true },
   basePath: "/Planet-Net-Zero",
 };
