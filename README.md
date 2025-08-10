c38bcc2e7a45be22fd03e807cc915264e73cd78d
Rashmi Suralkar <prashmi.89@gmail.com>
2025-08-10 13:11:24 +0100

    config changes

--- README.md
+++ README.md
@@ -2,6 +2,9 @@ Planet-Net-Zero
 
 
 
+Change next.confog.js  before prod deploy
+basepath
+
 command for static files generation
 npm run deploy
 
--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  basePath: "/Planet-Net-Zero",
 };
 
 module.exports = nextConfig;
